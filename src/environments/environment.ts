6847b3a23583b5313c416f66b336bb84530401f0
hasibul-99 <hasibul_uap@outlook.com>
2022-06-21 21:35:42 +0600

    update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,6 +9,6 @@ export const environment = {
   envName: 'DEV',
   test: false,
   i18nPrefix: '',
-  baseURL: 'http://property-api.planet0088.com/api/v1'
+  baseURL: 'https://api-property.planet0088.com/api/v1'
   // baseURL: 'https://rentals.robisignals.com/backend/api/v1'
 };
