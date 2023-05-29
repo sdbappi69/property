9384f444def0a348c311530c6c6a58380eef9cde
hasibul-99 <hasibul_uap@outlook.com>
2023-05-29 22:17:54 +0600

    update

--- src/app/shared/enums/extra-charge-type-enum.ts
+++ src/app/shared/enums/extra-charge-type-enum.ts
@@ -1,5 +1,6 @@
 export const EXTRA_CHARGE_TYPES = [
     { key: 'fixed', value: 'Fixed Value' },
     { key: 'total_rent_percentage', value: '% of Total Rent' },
-    { key: 'total_rent_over_due_percentage', value: '% of Total Amount Over Due' }
+    { key: 'total_rent_over_due_percentage', value: '% of Total Amount Over Due' },
+    { key: 'per_square_feet', value: 'Per Square Feet' }
 ];
